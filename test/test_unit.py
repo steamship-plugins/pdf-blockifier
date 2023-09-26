303cff239f8eefe1f4a92bb85bf518bb3a95c54d
Douglas Reid <douglas-reid@users.noreply.github.com>
2023-09-26 17:17:33 +0000

    cleanup

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,10 +1,7 @@
 """Test pdf-blockifier via unit tests."""
 from test import TEST_DATA
-from test.utils import load_config, verify_response
 
-import pytest as pytest
 from steamship import Steamship
-from steamship.base import TaskState
 from steamship.plugin.inputs.raw_data_plugin_input import RawDataPluginInput
 from steamship.plugin.request import PluginRequest
 
@@ -17,21 +14,18 @@ def _read_test_pdf_file(filename: str) -> str:
 
 
 def test_blockifier():
-    """Test AssemblyAI (S2T) Blockifier without edge cases."""
-    config = load_config()
-    client = Steamship()
-    blockifier = PdfBlockifier(client=client, config=config)
-    request = PluginRequest(
-        data=RawDataPluginInput(
-            data=_read_test_pdf_file("test.pdf"), default_mime_type="application/pdf"
+    """Test PDF Blockifier without edge cases."""
+    with Steamship.temporary_workspace() as client:
+        blockifier = PdfBlockifier(client=client)
+        request = PluginRequest(
+            data=RawDataPluginInput(
+                data=_read_test_pdf_file("test.pdf"), default_mime_type="application/pdf"
+            )
         )
-    )
-    response = blockifier.run(request)
+        response = blockifier.run(request)
 
-    blocks = response.data.file.blocks
-    assert blocks
-    assert len(blocks) == 2
-    assert blocks[0].text == "This is the ﬁrst page\n"
-    assert blocks[1].text == "This is the second page"
-
-
+        blocks = response.data.file.blocks
+        assert blocks
+        assert len(blocks) == 2
+        assert blocks[0].text == "This is the ﬁrst page\n"
+        assert blocks[1].text == "This is the second page"
